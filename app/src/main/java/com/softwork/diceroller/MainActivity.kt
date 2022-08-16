6a8b28d987318aa68df6d2986355b30945562461
Blakdothraki <blakdothraki@github.com>
2022-08-17 00:58:44 +0100

    First Commit; Raw dice roller app

---
+++ app/src/main/java/com/softwork/diceroller/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.softwork.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import com.softwork.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DiceRoller()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceRoller() {
+    ButtonAndImage(modifier = Modifier
+        .background(Color.Black)
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)) {
+
+    }
+}
+@Composable
+fun ButtonAndImage(modifier: Modifier = Modifier, function: () -> Unit) {
+    var result by remember { mutableStateOf(1)}
+    var image = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = result.toString()
+        )
+        Button(onClick = { result = (1..6).random()}, modifier = Modifier.background(Color.Green)) {
+            Text(text = stringResource(R.string.roll))
+        }
+
+    }
+}
+
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+fun DefaultPreview() {
+    DiceRollerTheme {
+        DiceRoller()
+    }
+}
